be3626ec03c7ff89d6f7fc8aa033cc4d67d7835c
rita <AdaRitDN@mail.ru>
2023-01-19 17:03:51 +0300

    двум массив заполненный случ числами

---
+++ qwerty8/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+// 1/  Создание маcсива static double[,] CreateArray(int m, int n)
+ double[,] CreateArray(int m, int n)
+        {
+            Random rnd = new Random(); // генератор случайных чисел
+
+            double[,] myArray = new double[m, n];
+
+            for (int i = 0; i < m; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    myArray[i, j] = Math.Round(rnd.NextDouble(), 2);
+                }
+
+            return myArray;
+        }   
+Console.WriteLine("Введите m:");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите n:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            var myArray = CreateArray(m, n);
+
+            for (int i = 0; i < m; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{myArray[i, j]}   ");
+                }
+            }
+
+            Console.ReadLine();
